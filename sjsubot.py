931c8a26fc1eb85380b8eced377ecbf9206f9a45
suchishree29 <suchishree.jena29@gmail.com>
2017-05-03 11:50:20 -0700

    Update sjsubot.py

--- sjsubot.py
+++ sjsubot.py
@@ -19,6 +19,12 @@ def handle_command(command, channel):
         are valid commands. If so, then acts on the commands. If not,
         returns back what it needs for clarification.
     """
-
+    print "In handle_command ********"
+    response = "Not sure what you mean. Use the *" + EXAMPLE_COMMAND + \
+               "* command with numbers, delimited by spaces."
+    if command.startswith(EXAMPLE_COMMAND):
+        response = "Sure...write some more code then I can do that!"
+    slack_client.api_call("chat.postMessage", channel=channel,
+                          text=response, as_user=True)
 
 
