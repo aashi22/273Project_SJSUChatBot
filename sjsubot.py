1f9e81ba2f884d50eac9988f0f2c94180a1198ac
suchishree29 <suchishree.jena29@gmail.com>
2017-05-03 11:50:09 -0700

    Update sjsubot.py

--- sjsubot.py
+++ sjsubot.py
@@ -7,3 +7,18 @@ BOT_ID = os.environ.get('BOT_ID')
 print BOT_ID
 # constants
 AT_BOT = "<@" + BOT_ID + ">"
+
+EXAMPLE_COMMAND = "do"
+
+# instantiate Slack
+slack_client = SlackClient(os.environ.get('SLACK_BOT_TOKEN'))
+
+def handle_command(command, channel):
+    """
+        Receives commands directed at the bot and determines if they
+        are valid commands. If so, then acts on the commands. If not,
+        returns back what it needs for clarification.
+    """
+
+
+
