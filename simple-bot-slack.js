db45a85c9fef41735f98b571d684c4e8c1eb1cc2
suchishree29 <suchishree.jena29@gmail.com>
2017-05-10 18:52:51 -0700

    Add files via upload

---
+++ simple-bot-slack.js
@@ -0,0 +1,33 @@
+require('dotenv').load();
+
+var Botkit = require('botkit');
+var express = require('express');
+var middleware = require('botkit-middleware-watson')({
+  username: process.env.CONVERSATION_USERNAME,
+  password: process.env.CONVERSATION_PASSWORD,
+  workspace_id: process.env.WORKSPACE_ID,
+  version_date: '2016-09-20'
+});
+
+// Configure your bot.
+var slackController = Botkit.slackbot();
+var slackBot = slackController.spawn({
+  token: process.env.SLACK_TOKEN
+});
+slackController.hears(['.*'], ['direct_message', 'direct_mention', 'mention'], function(bot, message) {
+  slackController.log('Slack message received');
+  middleware.interpret(bot, message, function(err) {
+    if (!err)
+		  bot.reply(message, message.watsonData.output.text.join('\n'));
+	});
+});
+
+slackBot.startRTM();
+
+// Create an Express app
+var app = express();
+var port = process.env.PORT || 5000;
+app.set('port', port);
+app.listen(port, function() {
+  console.log('Client server listening on port ' + port);
+});
